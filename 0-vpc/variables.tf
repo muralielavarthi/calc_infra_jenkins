0df855e13e6a0b04e5284481733e32143e49df86
Murali Elavarthi <muralis2p@gmail.com>
2026-04-21 14:16:04 +0530

    c

--- 0-vpc/variables.tf
+++ 0-vpc/variables.tf
@@ -1,23 +1,23 @@
 
 variable "vpc_cidr" {
-    
+    default = "10.0.0.0/16"
   }
   
 variable "public_subnet_cidr_1a" {
-    
+    default = "10.0.1.0/24"
   }
 variable "private_subnet_cidr_1a" {
-    
+    default = "10.0.2.0/24"
   }
 variable "public_subnet_cidr_1b" {
-  
+    default = "10.0.3.0/24"
 }
 variable "private_subnet_cidr_1b" {
-  
+    default = "10.0.4.0/24"
 }
 variable "database_subnet_cidr_1a" {
-  
+  default = "10.0.5.0/24"
 }
 variable "database_subnet_cidr_1b" {
-  
+  default = "10.0.6.0/24"
 }
